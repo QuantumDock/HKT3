613a8b46f3d311bc7c796f17ea95c7b21c562f30
Skyblueborb <tomaszborbely0710@gmail.com>
2023-05-27 00:58:04 +0200

    styles: rework high-contrast theme

--- static/js/contrast.js
+++ static/js/contrast.js
@@ -3,6 +3,14 @@ var toggleButton = document.getElementById("toggle-high-contrast");
 toggleButton.addEventListener("click", function () {
   // Get the body element
   var body = document.getElementsByTagName("body")[0];
+  var grid = document.querySelectorAll(".grid__element");
+  var navbar = document.querySelector(".navbar");
+  var navbar_button = document.querySelector(".navbar__button");
   // Toggle the "high-contrast" class on the body element
+  grid.forEach((gridElement) => {
+    gridElement.classList.toggle("high-contrast--special");
+  });
+  navbar.classList.toggle("high-contrast--special");
+  navbar_button.classList.toggle("high-contrast");
   body.classList.toggle("high-contrast");
 });
