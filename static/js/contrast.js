1cbab1587622e648fd64b380e931ce4f2a49acfd
Skyblueborb <tomaszborbely0710@gmail.com>
2023-05-27 08:22:14 +0200

    styles: a lot of css changes and improved high-contrast mode

--- static/js/contrast.js
+++ static/js/contrast.js
@@ -1,15 +1,30 @@
-function check(){
-  var storedValue = sessionStorage.getItem('theme');
-  if(storedValue == '1'){
+function check() {
+  var storedValue = sessionStorage.getItem("theme");
+  if (storedValue == "1") {
     toggle(false);
   }
 }
-function toggle(special){
+function toggle(special) {
   var body = document.getElementsByTagName("body")[0];
   var grid = document.querySelectorAll(".grid__element");
   var navbar = document.querySelector(".navbar");
   var navbar_button = document.querySelector(".navbar__button");
   var navbar_page = document.querySelector(".navbar__main-page");
+
+  try {
+    var scam = document.querySelectorAll(".grid__scam--h1");
+    scam.forEach((i) => {
+      i.classList.toggle("high-contrast--special");
+    });
+  } catch (TypeError) {
+    //pass
+  }
+  try {
+    var img_contrast = document.querySelector(".title__img");
+    img_contrast.src = "/static/img/senior_contrast.png";
+  } catch (TypeError) {
+    //pass
+  }
   try {
     var questions = document.querySelector(".questions");
     questions.classList.toggle("high-contrast--special");
@@ -17,27 +32,36 @@ function toggle(special){
     //pass
   }
   try {
-      var times = document.querySelectorAll(".hc-time");
-        times.forEach((i) => {
-          i.classList.toggle("high-contrast--special");
-        });
-      times.classList.toggle("high-contrast--special");
-    } catch (TypeError) {
-      //pass
-    }
-  if(special){
-    var storedValue = sessionStorage.getItem('theme');
-    sessionStorage.setItem('theme', (storedValue == 1 ? 0 : 1));
+    var times = document.querySelectorAll(".hc-time");
+    times.forEach((i) => {
+      i.classList.toggle("high-contrast--special");
+    });
+  } catch (TypeError) {
+    //pass
   }
-  // Toggle the "high-contrast" class on the body element
-  grid.forEach((gridElement) => {
-    gridElement.classList.toggle("high-contrast--special");
-  });
+  try {
+    var title__heading = document.querySelector(".title__heading");
+    title__heading.classList.toggle("high-contrast");
+  } catch (TypeError) {
+    //pass
+  }
+  try {
+    var grid = document.querySelectorAll(".grid__element");
+    grid.forEach((gridElement) => {
+      gridElement.classList.toggle("high-contrast--special");
+    });
+  } catch (TypeError) {
+    //pass
+  }
+  if (special) {
+    var storedValue = sessionStorage.getItem("theme");
+    sessionStorage.setItem("theme", storedValue == 1 ? 0 : 1);
+  }
   navbar.classList.toggle("high-contrast--special");
   navbar_button.classList.toggle("high-contrast");
   navbar_page.classList.toggle("high-contrast");
   body.classList.toggle("high-contrast");
-  }
+}
 var toggleButton = document.getElementById("toggle-high-contrast");
 toggleButton.addEventListener("click", function () {
   toggle(true);
