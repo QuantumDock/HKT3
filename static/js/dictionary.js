0ac114ce63abfdb3b5017f5a250b99b1cc22b2b3
Skyblueborb <tomaszborbely0710@gmail.com>
2023-05-27 09:37:41 +0200

    FIXUP!: fix dictionary on a linux system

--- static/js/dictionary.js
+++ static/js/dictionary.js
@@ -1,37 +1,42 @@
-function fileread(){
-    fetch("static/html_dictionary.csv")
-      .then(function(response) {
-        if (response.ok) {
-          return response.text();
-        } else {
-          throw new Error('File load error: ' + response.status);
-        }
-      })
-      .then(function(fileContents) {
-        let input = document.getElementById("dictionary__input").value;
-        let dataArray = fileContents.split('\r\n').map(function(line) {
-          return line.split(';'); // Split each line by comma
-        });
-        console.log(input)
-        if(input.includes("tunak")){ /* you found the easter egg */
-            window.location.replace("https://www.youtube.com/watch?v=IFP3Jc5ztgg");
-        }
-        let phrase = "";
-        let explanation = "";
-        for(i = 0; i < dataArray.length; i++){
-            if(input.toLowerCase().includes(dataArray[i][0].trim().toLowerCase())){
-                phrase = dataArray[i][1];
-                explanation = dataArray[i][2];
-            }
-            console.log(dataArray[i][0]);
-        }
-        document.getElementById("placeholder-title").textContent = phrase;
-        document.getElementById("placeholder-text").textContent = explanation;
-        if(phrase != ""){
-            document.getElementById("placeholder-dash").textContent = " - ";
-        }
-      })
-      .catch(function(error) {
-        console.error('File load error:', error);
+function fileread() {
+  fetch("static/html_dictionary.csv")
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else {
+        throw new Error("File load error: " + response.status);
+      }
+    })
+    .then(function (fileContents) {
+      let input = document.getElementById("dictionary__input").value;
+      let dataArray = fileContents.split("\n").map(function (line) {
+        return line.split(";"); // Split each line by comma
       });
+      console.log(input);
+      if (input.includes("tunak")) {
+        /* you found the easter egg */
+        window.location.replace("https://www.youtube.com/watch?v=IFP3Jc5ztgg");
+      }
+      let phrase = "";
+      let explanation = "";
+      console.log(dataArray.length);
+      for (i = 0; i < dataArray.length; i++) {
+        if (
+          input.toLowerCase().includes(dataArray[i][0].trim().toLowerCase())
+        ) {
+          phrase = dataArray[i][1];
+          explanation = dataArray[i][2];
+        }
+        console.log(dataArray[i][0]);
+      }
+      document.getElementById("placeholder-title").textContent = phrase;
+      document.getElementById("placeholder-text").textContent = explanation;
+      if (phrase != "") {
+        document.getElementById("placeholder-dash").textContent = " - ";
+      }
+    })
+    .catch(function (error) {
+      console.error("File load error:", error);
+    });
 }
+
