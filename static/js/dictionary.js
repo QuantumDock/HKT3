dd8fbdb59b15617c5a13955b4dc21ef9c864ee24
crazyjerz <informatyka11111@wp.pl>
2023-05-27 04:48:14 +0200

    html, css, js: dictionary

--- static/js/dictionary.js
+++ static/js/dictionary.js
@@ -9,8 +9,24 @@ function fileread(){
       })
       .then(function(fileContents) {
         let input = document.getElementById("dictionary__input").value;
-        document.getElementById("placeholder").textContent = "";
-        console.log(fileContents);
+        let dataArray = fileContents.split('\r\n').map(function(line) {
+          return line.split(';'); // Split each line by comma
+        });
+        console.log(input)
+        let phrase = "";
+        let explanation = "";
+        for(i = 0; i < dataArray.length; i++){
+            if(input.toLowerCase().includes(dataArray[i][0].trim().toLowerCase())){
+                phrase = dataArray[i][1];
+                explanation = dataArray[i][2];
+            }
+            console.log(dataArray[i][0]);
+        }
+        document.getElementById("placeholder-title").textContent = phrase;
+        document.getElementById("placeholder-text").textContent = explanation;
+        if(phrase != ""){
+            document.getElementById("placeholder-dash").textContent = " - ";
+        }
       })
       .catch(function(error) {
         console.error('File load error:', error);
