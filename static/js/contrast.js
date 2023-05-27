10e78ccbe52bac0344a31a870d8649b27a6ed76f
Skyblueborb <tomaszborbely0710@gmail.com>
2023-05-27 09:03:14 +0200

    js: fixup contrast behavious with the logo icon

--- static/js/contrast.js
+++ static/js/contrast.js
@@ -6,11 +6,16 @@ function check() {
 }
 function toggle(special) {
   var body = document.getElementsByTagName("body")[0];
-  var grid = document.querySelectorAll(".grid__element");
   var navbar = document.querySelector(".navbar");
   var navbar_button = document.querySelector(".navbar__button");
   var navbar_page = document.querySelector(".navbar__main-page");
 
+  try {
+    var quiz_button = document.querySelector("#quiz_button");
+    quiz_button.classList.toggle("high-contrast");
+  } catch (TypeError) {
+    //pass
+  }
   try {
     var scam = document.querySelectorAll(".grid__scam--h1");
     scam.forEach((i) => {
@@ -21,7 +26,14 @@ function toggle(special) {
   }
   try {
     var img_contrast = document.querySelector(".title__img");
-    img_contrast.src = "/static/img/senior_contrast.png";
+    var text = img_contrast.src;
+    var pattern = ".*static/img/senior_contrast.png$";
+    var result = text.match(pattern);
+    if (result) {
+      img_contrast.src = "/static/img/senior.png";
+    } else {
+      img_contrast.src = "/static/img/senior_contrast.png";
+    }
   } catch (TypeError) {
     //pass
   }
